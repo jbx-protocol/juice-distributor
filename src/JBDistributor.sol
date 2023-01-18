a7428698ec6d5641ef31e0c9a435bc0b72d81859
DrGorilla_md <dr_gorilla@protonmail.com>
2023-01-19 00:14:56 +0100

    init

---
+++ src/JBDistributor.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+/**
+ * @title   JBDistributor
+ * @notice 
+ * @dev 
+ */
+contract JBDistributor {
+
+// INSERT JUICY CODE HERE
+
+
+}
---
+++ test/JBDistributor.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "forge-std/Test.sol";
+import "../src/JBDistributor.sol";
+
+contract JBDistributorTest is Test {
+    JBDistributor public distributor;
+
+    function setUp() public {
+    }
+}
